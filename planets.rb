10eefa6840de92544e0e3b4a61800034601784c8
Jasmine Quach <xjasminequach@gmail.com>
2017-10-07 19:35:59 -0500

    Update README.md

---
+++ planets.rb
@@ -0,0 +1,19 @@
+planet_list = ["Mercury", "Mars",]
+
+
+
+
+planet_list.each do |planet|
+  Spacecraft.each do |craft|
+    if planet == craft[1]
+      puts "#{craft[0] #{planet}"
+    end
+  end
+end
+
+
+rocky = plane_list[0..3]
+.pop[Pluto
+
+]
+
