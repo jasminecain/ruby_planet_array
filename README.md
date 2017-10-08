60737cef7c074901881ae39a07628bb8f49cceb7
Jasmine Quach <xjasminequach@gmail.com>
2017-10-07 20:58:05 -0500

    Complete exercise

--- README.md
+++ README.md
@@ -18,7 +18,8 @@ Use insert() to add Earth, and Venus in the correct order.
 Use << ("shovel operator") to add Pluto to the end of the array.
 Now that all the planets are in the array, slice the array in order to get the rocky planets into a new list called rocky_planets.
 Being good amateur astronomers, we know that Pluto is now a dwarf planet, so use the pop operation to remove it from the end of planet_list.
-Iterating over planets
+
+###Iterating over planets
 
 Create another array containing arrays where each array will hold the name of a spacecraft that we have launched, and the names of the planet(s) that it has visited, or landed on. (e.g. ['Cassini', 'Saturn']).
 Iterate over your array of planets, and inside that loop, iterate over the array of arrays. Print, for each planet, which satellites have visited.
--- planets.rb
+++ planets.rb
@@ -1,19 +1,30 @@
 planet_list = ["Mercury", "Mars",]
 
+puts planet_list.push("Jupiter", "Saturn")
+puts "Push Method: #{planet_list}"
+
+puts planet_list.concat([ "Uranus", "Neptune" ])
+puts "Concat Method: #{planet_list}"
+
+puts planet_list.insert(1, "Venus").insert(2, "Earth")
+puts "Insert Method: #{planet_list}"
+
+puts planet_list << "Pluto"
+puts "Pop Pluto in: #{planet_list}"
+
+rocky_planets = planet_list[0..3]
+puts "Rocky Planets: #{rocky_planets}"
+
+puts planet_list.pop["Pluto"]
+puts "Plutoless: #{planet_list}"
 
 
+spacecraft_launched = [ ["Messenger", "Mercury"], ["Marier2", "Venus"], ["Mariner10", "Earth"], ["Viking1", "Mars"], ["Juno", "Jupiter"], ["Cassini", "Saturn"], ["Voyager2", "Uranus"], ["Voyager2", "Neptune"] ]
 
 planet_list.each do |planet|
-  Spacecraft.each do |craft|
+  spacecraft_launched.each do |craft|
     if planet == craft[1]
-      puts "#{craft[0] #{planet}"
+      puts "Launced Spacecrafts: #{craft[0]} #{planet}"
     end
   end
 end
-
-
-rocky = plane_list[0..3]
-.pop[Pluto
-
-]
-
